6dc9644d394c65102b6c526ff56defb93d006a16
Siwei Xu <xusiwei1236@163.com>
2016-04-10 22:25:36 +0800

    comments in Lua

---
+++ basis/comments.lua
@@ -0,0 +1,19 @@
+-- single line comments
+print("well")
+
+--[[ muli-line
+comments
+print("not shown")
+]]
+
+
+
+-- a common trick to switch a piece of code
+
+--[[
+print(10)   -- no actions
+--]]
+
+---[[
+print(10)   -- 10 
+--]]
