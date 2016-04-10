6210101c7b6e1715199b8f2ccdf9beda0426f8bc
Siwei Xu <xusiwei1236@163.com>
2016-04-11 00:10:06 +0800

    numbers misc

---
+++ basis/numbers.lua
@@ -0,0 +1,18 @@
+print(1/7)
+print(1/7*7)
+print(12.7 - 20 + 7.3)
+print()
+
+print(4)
+print(4.0)
+print(0.4)
+print(4.57e-3)
+print(0.3e12)
+print(5e20)
+print()
+
+print(0xff)    --> 255
+print(0x1a3)   --> 419
+print(0x0.2)   --> 0.125
+print(0x1p-1)  --> 0.5
+print(0xa.bp2) --> 42.75
