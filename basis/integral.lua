b65fe6c4364b3ab47ed4a20539fe05c56a1143ea
Siwei Xu <xusiwei1236@163.com>
2016-04-15 21:14:45 +0800

    colsure examples

---
+++ basis/integral.lua
@@ -0,0 +1,33 @@
+function integral(f, delta)
+  delta = delta or 1e-6
+  return function(a, b)
+           local sum, count = 0, (b-a) / delta
+           for i = 1, count do
+             sum = sum + f(a + i*delta)*delta
+           end
+           return sum
+         end
+end
+
+f = function(x) return x end
+g = integral(f)
+print(g(0, 1))
+
+
+function polynomial(a)
+  return function(x)
+           local sum = 0
+           for i = 1, #a do
+             sum = sum * x
+             sum = sum + a[i]
+           end
+           return sum
+         end
+end
+f = polynomial({3, 0, 1})
+print(f(0))   --> 1
+print(f(5))   --> 76
+print(f(10))  --> 301
+
+
+
